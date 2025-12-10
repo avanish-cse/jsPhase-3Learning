fee73621b7348dcf7f7362ccbd5cb21c9c8e8aa9
Avanish Chaurasiya <avanishchaurasiya71@gmail.com>
2025-12-10 13:10:51 +0530

    add checkRole method

--- 4.OOPs-and-classes/script.js
+++ 4.OOPs-and-classes/script.js
@@ -1,40 +1,47 @@
 class User {
-    constructor(name, address, username, email){
+    constructor(name, address, username, email) {
         this.name = name;
         this.address = address;
         this.username = username;
         this.email = email;
         this.role = "user";
-        
+
     }
 
-write(text){
-    let h1 = document.createElement("h1");
-    h1.textContent = `${this.name} : ${text}`;
-    document.body.appendChild(h1);
-}
+    write(text) {
+        let h1 = document.createElement("h1");
+        h1.textContent = `${this.name} : ${text}`;
+        document.body.appendChild(h1);
+    }
+
+    checkRole() {
+      return  this.role !== "user" ? `you are an ${this.role}`: `you are a ${this.role}`;
+      
+    }
 }
 
-let user1 = new User("Avanish" , "Khalilabad", "avanish.71", "avanishchaurasiya71@gmail.com");
-let user2 = new User("Ankita" , "Khalilabad", "akieve.4", "idk04@gmail.com");
+let user1 = new User("Avanish", "Khalilabad", "avanish.71", "avanishchaurasiya71@gmail.com");
+let user2 = new User("Ankita", "Khalilabad", "akieve.4", "idk04@gmail.com");
 
 
 // extends
 
 class Admin extends User {
-    constructor(name, address, username, email){
+    constructor(name, address, username, email) {
         super(name, address, username, email);
         this.role = "admin";
 
     }
 
     remove() {
-document.querySelectorAll("h1").forEach( (element)=> {
-element.remove();
-})
+        document.querySelectorAll("h1").forEach((element) => {
+            element.remove();
+        })
     }
+
+   
 }
 
 
 
-let admin1 = new Admin("admin", "mai nhi bataunga" , "@admin", "admin@mail.com");
+let admin1 = new Admin("admin", "mai nhi bataunga", "@admin", "admin@mail.com");
